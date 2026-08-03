e998388a490e5f1873f55cca5997005eb5c42713
robertjonsonss23-rgb <robert.jonsonss23@gmail.com>
2026-08-03 23:05:51 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import type {
   RunStatus,
 } from "./types/order";
 import {
-  updateOrderControl,
+  updateOrderControl, 
   fetchOrderStatus,
   normalizePlatform,
   fetchOrdersForCurrentUser,
